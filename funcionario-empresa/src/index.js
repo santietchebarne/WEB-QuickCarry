85b55519b6d794d65b4722d2627192ee47ff165f
Pablo Rodriguez <pablordzinfo@gmail.com>
2023-11-12 18:41:54 -0300

    modificando lógica de obtención de token

--- funcionario-empresa/src/index.js
+++ funcionario-empresa/src/index.js
@@ -1,17 +1,9 @@
-const tokenLocalStorage = localStorage.getItem('token')
+const urlParams = new URLSearchParams(window.location.search)
+const token = urlParams.get('token')
 
-if (!tokenLocalStorage) {
-    const urlParams = new URLSearchParams(window.location.search)
-    const token = urlParams.get('token')
-
-    if(!token) {
-        location.href = 'http://localhost:5000'
-    } else {
-        localStorage.setItem('token', token)
-        location.href = '/html/profile.html'
-    }
+if (!token) {
+    location.href = 'http://localhost:5000'
 } else {
+    localStorage.setItem('token', token)
     location.href = '/html/profile.html'
 }
-
-
